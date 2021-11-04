11415f3f2ed2f3d40b85f95f58328ad20a119c56
syzdev <syzdev@gmail.com>
2021-11-04 20:24:12 +0800

    调整Cesium初始化代码

--- src/components/CesiumScene.vue
+++ src/components/CesiumScene.vue
@@ -6,40 +6,19 @@
 // import Cesium from 'cesium/Source/Cesium'
 import * as Cesium from 'cesium'
 import 'cesium/Source/Widgets/widgets.css'
-import { CesiumIonDefaultAccessToken, TianDiTuToken } from '../common/js/config'
+import { initCesium } from '../common/js/initCesium'
 export default {
   mounted() {
-    Cesium.Ion.defaultAccessToken = CesiumIonDefaultAccessToken
-    let viewer = new Cesium.Viewer('cesiumContainer', {
-      shouldAnimate: true,
-      animation: false, // 是否创建动画小器件，左下角仪表
-      baseLayerPicker: false, // 是否显示图层选择器
-      fullscreenButton: false, // 是否显示全屏按钮
-      geocoder: false, // 是否显示geocoder小器件，右上角查询按钮
-      homeButton: false, // 是否显示Home按钮
-      infoBox: false, // 是否显示信息框
-      sceneModePicker: false, // 是否显示3D/2D选择器
-      selectionIndicator: false, // 是否显示选取指示器组件
-      timeline: false, // 是否显示时间轴
-      navigationHelpButton: false, // 是否显示右上角的帮助按钮
-      scene3DOnly: false, // 如果设置为true，则所有几何图形以3D模式绘制以节约GPU资源
-      imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-        url: `http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=${TianDiTuToken}`,
-        layer: 'tdtBasicLayer',
-        style: 'default',
-        format: 'image/jpeg',
-        tileMatrixSetID: 'GoogleMapsCompatible',
-      }),
-    })
-    // window.viewer = viewer
-    viewer._cesiumWidget._creditContainer.style.display = "none"
+    let viewer = initCesium(Cesium)
+    viewer._cesiumWidget._creditContainer.style.display = 'none' // 隐藏版权信息
   },
-  methods: {},
 }
 </script>
 
 <style>
-html, body, #cesiumContainer {
+html,
+body,
+#cesiumContainer {
   height: 100%;
   width: 100%;
   margin: 0;
